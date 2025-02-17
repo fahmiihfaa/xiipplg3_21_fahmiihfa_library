003aaffdf0872561c7bdf76d6887d4c59484f220
fahmiihfaa <fahmiikhfa1996@gmail.com>
2025-02-17 09:55:36 +0700

    tabel review

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const userreviews = require('./router/reviews')
+const db = require("./config/db"); // Import koneksi database dari config/db.js
+
+const port = 3000
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.get('/reviews', (req, res) => {
+  db.query("SELECT * FROM reviews", (err, results) => {
+    if (err) {
+        return res.status(500).json({ error: err.message }); // Jika terjadi error, kirim respons 500
+    }
+    res.json(results); // Kirim hasil query sebagai JSON ke client
+});
+})
+
+app.use(userreviews)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
